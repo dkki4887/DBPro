5f6b8f67a239301ca6d38cfc1be357b98bfc78e5
comboong2 <mhtiger1014@naver.com>
2022-11-12 01:13:09 +0900

    주문조회 cmit

--- src/main/java/persistence/dto/OrderMenuDTO.java
+++ src/main/java/persistence/dto/OrderMenuDTO.java
@@ -10,6 +10,6 @@ import org.apache.ibatis.type.Alias;
 @ToString
 public class OrderMenuDTO {
     private int orderMenu_id;
-    private int menu_name;
+    private String menu_name;
     private long menu_price;
 }
