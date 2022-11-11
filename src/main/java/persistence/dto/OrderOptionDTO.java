3c6cf5ab0d69ca0c0f4f4d2b0ec85541b5b586d1
dkki4887 <4887chaeg@gmail.com>
2022-11-11 19:35:18 +0900

    MyDAO

---
+++ src/main/java/persistence/dto/OrderOptionDTO.java
@@ -0,0 +1,16 @@
+package persistence.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.apache.ibatis.type.Alias;
+
+@Getter
+@Setter
+@ToString
+@Alias("OrderOptionDTO")
+public class OrderOptionDTO {
+    private int order_id;
+    private String option_name;
+    private long option_price;
+}
