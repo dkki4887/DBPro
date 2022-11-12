fe07c753e35fdfe1c19e8d344358e4ce1f5f1d7c
Mar1boroo <dnld1759@gmail.com>
2022-11-13 02:19:32 +0900

    review insert > auto key 추가

--- src/main/java/Function/CustomerFunction.java
+++ src/main/java/Function/CustomerFunction.java
@@ -1,8 +1,11 @@
 package Function;
 
 import persistence.MyBatisConnectionFactory;
+import persistence.dao.MyOrderDAO;
 import persistence.dao.MyReviewDAO;
+import service.OrderService;
 import service.ReviewService;
+import view.OrderView;
 import view.ReviewView;
 
 public class CustomerFunction
@@ -10,16 +13,31 @@ public class CustomerFunction
     private MyReviewDAO myReviewDAO;
     private ReviewService reviewService;
     private ReviewView reviewView;
+
+    private MyOrderDAO myOrderDAO;
+    private OrderService orderService;
+    private OrderView orderView;
+
     public CustomerFunction()
     {
         this.myReviewDAO = new MyReviewDAO(MyBatisConnectionFactory.getSqlSessionFactory());
         this.reviewService = new ReviewService(myReviewDAO);
         this.reviewView = new ReviewView();
+
+        this.myOrderDAO = new MyOrderDAO(MyBatisConnectionFactory.getSqlSessionFactory());
+        this.orderService = new OrderService(myOrderDAO);
+        this.orderView = new OrderView();
     }
 
-    public void writeReview(int review_id , int store_id , String user_id , int order_id)
+    public void inquireOrder(String customer_id)
     {
-        int reviewResult=reviewService.insertReview(review_id,store_id,user_id,order_id);
+        orderView.printOrder( orderService.selectOrder_customer(customer_id) );
+    }
+
+
+    public void writeReview(int store_id , String user_id , int order_id)
+    {
+        int reviewResult=reviewService.insertReview(store_id,user_id,order_id);
         if(reviewResult ==1) System.out.println("리뷰 작성 성공");
         else System.out.println("리뷰 작성 실패");
     }
