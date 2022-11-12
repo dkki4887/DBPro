89b08e8a59d14e404b82afd21b4b6622e2abc978
ttottochi <117351120+ttottochi@users.noreply.github.com>
2022-11-12 20:55:43 +0900

    user add clear

--- src/main/java/persistence/dao/MyUserDAO.java
+++ src/main/java/persistence/dao/MyUserDAO.java
@@ -2,6 +2,7 @@ package persistence.dao;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import persistence.dto.MenuDTO;
 import persistence.dto.UserDTO;
 
 import java.util.List;
@@ -21,6 +22,33 @@ public class MyUserDAO {
             session.close();
         }
         return list;
+    }
+
+    public List<UserDTO> selectAllUserid(){
+        List<UserDTO> list = null;
+        SqlSession session = sqlSessionFactory.openSession();
+        try{
+            list = session.selectList("mapper.UserMapper.selectAllUserid");
+        } finally {
+            session.close();
+        }
+        return list;
+    }
+
+    public void userAdd(UserDTO userDTO){
+        SqlSession session = sqlSessionFactory.openSession();
+        int result = -1;
+        try{
+            result = session.selectOne("mapper.MenuMapper.userAdd", userDTO);
+
+            if (result==1){
+                session.commit();
+            }else {
+                session.rollback();
+            }
+        } finally {
+            session.close();
+        }
     }
     public UserDTO selectById(Long id){return null;}
 }
