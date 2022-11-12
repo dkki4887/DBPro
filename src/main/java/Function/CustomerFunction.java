e4ac03f827a4bfc1c694ecb92a7d8b553727628c
Mar1boroo <dnld1759@gmail.com>
2022-11-13 00:21:26 +0900

    CustomerFunction - 리뷰 추가

--- src/main/java/Function/CustomerFunction.java
+++ src/main/java/Function/CustomerFunction.java
@@ -1,4 +1,35 @@
 package Function;
 
-public class CustomerFunction {
+import persistence.MyBatisConnectionFactory;
+import persistence.dao.MyReviewDAO;
+import service.ReviewService;
+import view.ReviewView;
+
+public class CustomerFunction
+{
+    private MyReviewDAO myReviewDAO;
+    private ReviewService reviewService;
+    private ReviewView reviewView;
+    public CustomerFunction()
+    {
+        this.myReviewDAO = new MyReviewDAO(MyBatisConnectionFactory.getSqlSessionFactory());
+        this.reviewService = new ReviewService(myReviewDAO);
+        this.reviewView = new ReviewView();
+    }
+
+    public void writeReview(int review_id , int store_id , String user_id , int order_id)
+    {
+        int reviewResult=reviewService.insertReview(review_id,store_id,user_id,order_id);
+        if(reviewResult ==1) System.out.println("리뷰 작성 성공");
+        else System.out.println("리뷰 작성 실패");
+    }
+
+    public void inquireReview(String user_id)
+    {
+        reviewView.printMyReview( reviewService.findReviewWithUserIdLike( user_id ) );
+    }
+
+
+
+
 }
