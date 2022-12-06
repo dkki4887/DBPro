6bb62e0eee168b96c9da3e2ed0f0c9ba9f48105c
ttottochi <117351120+ttottochi@users.noreply.github.com>
2022-12-07 04:09:38 +0900

    헤더 추가 마지막 번호 0x5E

--- src/main/java/protocol/Header.java
+++ src/main/java/protocol/Header.java
@@ -76,6 +76,7 @@ public class Header implements MySerializableClass {
     public final static byte CODE_UPDATE_USER_INFO = 0x5B;
     public final static byte CODE_UPDATE_STORE_TIME = 0x5C;
     public final static byte CODE_INSERT_MENU_OPTION = 0x5D;
+    public final static byte CODE_MENU_OPTION = 0x5E;
 
 
 
